3ebaa190c450745e6d7c569ff3302cfffc6351c1
arielflima <arielflima@gmail.com>
2019-10-29 17:39:15 -0300

    propriedades do react, defalt props e proptypes

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+
+import TechItem from './TechItem';
+
 class TechList extends Component {
   state = {
     newTech: '',
@@ -12,33 +15,42 @@ class TechList extends Component {
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
-    }
+  }
 
   handleSubmit = e => {
     e.preventDefault();
 
-    this.setState({ 
+    this.setState({
       techs: [...this.state.techs, this.state.newTech],
       newTech: ''
-     });
+    });
+  }
+
+  handleDelete = (tech) => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) })
   }
 
   render() {
     console.log(this.state)
     return (
       <form onSubmit={this.handleSubmit}>
-      <h1>{this.state.newTech}</h1>
-      <ul>
-        {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
-      </ul>
-      <input 
-      type="text" 
-      onChange={this.handleInputChange} 
-      value={this.state.newTech} 
-      />
-      <button type="submit">Enviar</button>
+        <ul>
+          {this.state.techs.map(tech => (
+            <TechItem
+              key={tech}
+              tech={tech}
+              onDelete={() => this.handleDelete(tech)}
+            />
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+        <button type="submit">Enviar</button>
       </form>
-    );
+    )
   }
 }
 
